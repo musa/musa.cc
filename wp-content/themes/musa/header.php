929cdafb2b56d107f66b3a528242090065cf4260
Vilson Vieira <vilson@void.cc>
2010-08-19 01:55:20 -0300

    added linhas.pjs

--- wp-content/themes/musa/header.php
+++ wp-content/themes/musa/header.php
@@ -36,7 +36,7 @@ $colorScheme = get_bloginfo('template_directory')."/style-".$theme_options["colo
 </head>
 <!-- animacao de fundo com processingJS -->
 <body style="overflow: hidden;" onload="init();">
- <canvas datasrc="<?php bloginfo('template_directory'); ?>/pjs/sombra.pjs"> </canvas>
+ <canvas datasrc="<?php bloginfo('template_directory'); ?>/pjs/linhas.pjs"> </canvas>
 
 <!-- animacao de fundo com processingJS -->
 
