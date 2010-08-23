45bb0a18ac66736ffecd504634ee2b31ab664bba
Vilson Vieira <vilson@void.cc>
2010-08-23 19:54:57 -0300

    sadly, oh sad end... no more emotion... bye, bye sweet pee j.

--- wp-content/themes/musa/header.php
+++ wp-content/themes/musa/header.php
@@ -1,14 +1,14 @@
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head profile="http://gmpg.org/xfn/11">
 <meta http-equiv="Content-Type" content="<?php bloginfo('html_type'); ?>; charset=<?php bloginfo('charset'); ?>" />
-<title><?php if (function_exists('is_tag') && is_tag()) { 
-echo 'Posts tagged &quot;'.$tag.'&quot; - '; } 
-elseif (is_archive()) { wp_title(''); echo ' Archive - '; } 
-elseif (is_search()) { echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; } 
-elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' - '; } 
+<title><?php if (function_exists('is_tag') && is_tag()) {
+echo 'Posts tagged &quot;'.$tag.'&quot; - '; }
+elseif (is_archive()) { wp_title(''); echo ' Archive - '; }
+elseif (is_search()) { echo 'Search for &quot;'.wp_specialchars($s).'&quot; - '; }
+elseif (!(is_404()) && (is_single()) || (is_page())) { wp_title(''); echo ' - '; }
 elseif (is_404()) { echo 'Not Found - '; } bloginfo('name'); ?></title>
 <link rel="alternate" type="application/rss+xml" title="<?php bloginfo('name'); ?> RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
 <link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/reset.css" type="text/css" media="screen" />
@@ -28,19 +28,24 @@ $colorScheme = get_bloginfo('template_directory')."/style-".$theme_options["colo
 <?php if ( is_singular() ) wp_enqueue_script( 'comment-reply' ); ?>
 <?php wp_head(); ?>
 
-<!-- animacao de fundo com processingJS -->
+<!-- :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-(
+
+     infelizmente o mundo não está preparado para isso...
+
+     :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-( :-(
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/processing-0.9.7.min.js"></script>
 <script type="text/javascript" src="<?php bloginfo('template_directory'); ?>/js/init.js"></script>
-<!-- animacao de fundo com processingJS -->
+-->
 
 </head>
 <!-- animacao de fundo com processingJS -->
-
+<!--
 <body onload="init();">
 
 <canvas datasrc="<?php bloginfo('template_directory'); ?>/pjs/linhas.pjs"> </canvas>
+-->
 
-<!-- animacao de fundo com processingJS -->
+<body>
 
 <div id="main-wrapper">
 
